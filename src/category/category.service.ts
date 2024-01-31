0f91351829f44e784eda8253014b57f99bc3064e
mhadziqrazin <mhadziq0403@gmail.com>
2024-01-31 13:28:11 +0700

    feat: error handling for get all cateogries

--- src/category/category.service.ts
+++ src/category/category.service.ts
@@ -1,12 +1,27 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { ConnectionService } from 'src/connection/connection.service';
+
+const internalServerError = new HttpException(
+  'Something went wrong',
+  HttpStatus.INTERNAL_SERVER_ERROR,
+);
+
+const noCategoryWithSuchId = new HttpException(
+  'No category with such ID',
+  HttpStatus.BAD_REQUEST,
+);
 
 @Injectable()
 export class CategoryService {
   async getAllCategories() {
-    const prisma = ConnectionService.connectDb();
-    const categories = await prisma.category.findMany();
-    return categories;
+    try {
+      const prisma = ConnectionService.connectDb();
+      const categories = await prisma.category.findMany();
+      return categories;
+    } catch (err) {
+      console.log(err);
+      throw internalServerError;
+    }
   }
 
   async createCategory(name: string) {
