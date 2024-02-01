904535195f20f31970c538e099cfeca30b77a2de
mhadziqrazin <mhadziq0403@gmail.com>
2024-02-01 09:45:15 +0700

    feat: login auth

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,17 @@ import { AppService } from './app.service';
 import { ConnectionModule } from './connection/connection.module';
 import { QuoteModule } from './quote/quote.module';
 import { CategoryModule } from './category/category.module';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [ConnectionModule, QuoteModule, CategoryModule],
+  imports: [
+    ConnectionModule,
+    QuoteModule,
+    CategoryModule,
+    AuthModule,
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/auth/auth.service.ts
@@ -0,0 +1,26 @@
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { UsersService } from 'src/users/users.service';
+const bcrypt = require('bcryptjs');
+
+@Injectable()
+export class AuthService {
+  constructor(private readonly usersService: UsersService, private readonly jwtService: JwtService) {}
+
+  async signIn(username: string, password: string) {
+    const user = await this.usersService.findOne(username);
+
+    const valid = await bcrypt.compare(password, user.password);
+    if (!valid) {
+      throw new HttpException(
+        'Invalid password',
+        HttpStatus.UNAUTHORIZED,
+      );
+    }
+
+    const payload = { sub: user.username };
+    return {
+      access_token: await this.jwtService.signAsync(payload),
+    };
+  }
+}
