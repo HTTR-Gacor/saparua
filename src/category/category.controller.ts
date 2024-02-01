228de5b7fab267789edaaa6094c4c5ab27e34d44
mhadziqrazin <mhadziq0403@gmail.com>
2024-02-01 10:01:14 +0700

    refactor: run prettier fromat

--- src/category/category.controller.ts
+++ src/category/category.controller.ts
@@ -1,4 +1,12 @@
-import { Body, Controller, Delete, Get, Param, Post, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  UseGuards,
+} from '@nestjs/common';
 import { CategoryService } from './category.service';
 import { AuthGuard } from 'src/auth/auth.guard';
 
